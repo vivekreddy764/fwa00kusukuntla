17c67e9c4e33640c49b017df931bb2fa16960d4a
Saivivek Reddy <s544900@nwmissouri.edu>
2021-10-27 18:37:10 -0500

    updated bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var rno;
+var n1;
 var imul;
 var log;
 var log10;
@@ -8,20 +8,20 @@ var log10;
 /* GET users listing. */
 
 router.get("/", function (req, res, next) {
-  rno = Math.random() * (100 - 1) + 1;
-  rno1 = Math.random() * (100 - 1) + 1;
+  n1 = Math.random() * (100 - 1) + 1;
+  n2 = Math.random() * (100 - 1) + 1;
 
-  imul = Math.imul(rno, rno1);
+  imul = Math.imul(n1, n2);
 
-  log = Math.log(rno);
+  log = Math.log(n1);
 
-  log10 = Math.log10(rno);
+  log10 = Math.log10(n1);
 
-  res.send(`(imul) applied to (${rno}, ${rno1}) is ${imul} <br>
+  res.send(`(imul) applied to (${n1}, ${n2}) is ${imul} <br>
 
-            (log) applied to (${rno}) is ${log} <br>
+            (log) applied to (${n1}) is ${log} <br>
 
-            (log10) applied to (${rno}) is ${log10}`);
+            (log10) applied to (${n1}) is ${log10}`);
 });
 
 module.exports = router;
