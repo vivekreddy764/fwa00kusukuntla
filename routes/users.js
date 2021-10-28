fcda6c7106ee05ab0097701dd7e6a985bbd7487b
Saivivek Reddy <s544900@nwmissouri.edu>
2021-10-27 23:24:23 -0500

    updated users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
-
+var express = require("express"); 
+var router = express.Router(); 
+var value = 0;
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+router.get("/", function (req, res, 
+  next) { 
+    value += 1;
+    res.send(`User accessess are ${value}`); 
+  }); 
+  module.exports = router;
